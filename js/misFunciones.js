a7382880575d272429046a72ad60f5a66fe53037
John Jairo Mogollón Castillo <33044288+jotajotamcastillo@users.noreply.github.com>
2021-10-18 19:02:48 -0500

    FincaReservaFronted V1

--- js/misFunciones.js
+++ js/misFunciones.js
@@ -1,182 +1,301 @@
-function traerInformacionCategorias(){
+function traerInformacionCategorias() {
     $.ajax({
-        url:"http://129.151.120.79:8080/api/Category/all",
-        type:"GET",
-        datatype:"JSON",
-        success:function(respuesta){
+        url: "http://localhost:8080/api/Category/all",
+        type: "GET",
+        datatype: "JSON",
+        success: function (respuesta) {
             console.log(respuesta);
             pintarRespuesta(respuesta);
         }
     });
 }
 
-function pintarRespuesta(respuesta){
+function pintarRespuesta(respuesta) {
 
-    let myTable="<table>";
-    for(i=0;i<respuesta.length;i++){
-        myTable+="<tr>";
-        myTable+="<td>"+respuesta[i].name+"</td>";
-        myTable+="<td>"+respuesta[i].description+"</td>";
-        myTable+="</tr>";
+    let myTable = "<table>";
+    for (i = 0; i < respuesta.length; i++) {
+        myTable += "<tr>";
+        myTable += "<td>" + respuesta[i].name + "</td>";
+        myTable += "<td>" + respuesta[i].description + "</td>";
+        myTable += "</tr>";
     }
-    myTable+="</table>";
+    myTable += "</table>";
     $("#resultado1").html(myTable);
 }
 
-function guardarInformacionCategorias(){
+function guardarInformacionCategorias() {
     let var2 = {
-        name:$("#Cname").val(),
-        description:$("#Cdescription").val()
-        };
-      
-        $.ajax({
-        type:'POST',
+        name: $("#Cname").val(),
+        description: $("#Cdescription").val()
+    };
+
+    $.ajax({
+        type: 'POST',
         contentType: "application/json; charset=utf-8",
         dataType: 'JSON',
         data: JSON.stringify(var2),
-        
-        url:"http://129.151.120.79:8080/api/Category/save",
-       
-        
-        success:function(response) {
-                console.log(response);
+
+        url: "http://localhost:8080/api/Category/save",
+
+
+        success: function (response) {
+            console.log(response);
             console.log("Se guardo correctamente");
             alert("Se guardo correctamente");
             window.location.reload()
-    
+
         },
-        
-        error: function(jqXHR, textStatus, errorThrown) {
-              window.location.reload()
+
+        error: function (jqXHR, textStatus, errorThrown) {
+            window.location.reload()
             alert("No se guardo correctamente");
-    
-    
+
+
         }
-        });
+    });
 
 }
 
-///////////////////Bikes//////////////////////////////////////
-function traerInformacionBikes(){
+///////////////////Farms//////////////////////////////////////
+function traerInformacionFarm() {
     $.ajax({
-        url:"http://129.151.120.79:8080/api/Bike/all",
-        type:"GET",
-        datatype:"JSON",
-        success:function(respuesta){
+        url: "http://localhost:8080/api/Farm/all",
+        type: "GET",
+        datatype: "JSON",
+        success: function (respuesta) {
             console.log(respuesta);
-            pintarRespuestaBikes(respuesta);
+            pintarRespuestaFarm(respuesta);
         }
     });
 }
 
-function pintarRespuestaBikes(respuesta){
+function pintarRespuestaFarm(respuesta) {
 
-    let myTable="<table>";
-    for(i=0;i<respuesta.length;i++){
-        myTable+="<tr>";
-        myTable+="<td>"+respuesta[i].name+"</td>";
-        myTable+="<td>"+respuesta[i].brand+"</td>";
-        myTable+="<td>"+respuesta[i].year+"</td>";
-        myTable+="<td>"+respuesta[i].description+"</td>";
-        myTable+="</tr>";
+    let myTable = "<table>";
+    for (i = 0; i < respuesta.length; i++) {
+        myTable += "<tr>";
+        myTable += "<td>" + respuesta[i].name + "</td>";
+        myTable += "<td>" + respuesta[i].address + "</td>";
+        myTable += "<td>" + respuesta[i].extension + "</td>";
+        myTable += "<td>" + respuesta[i].description + "</td>";
+        myTable += "</tr>";
     }
-    myTable+="</table>";
+    myTable += "</table>";
     $("#resultado2").html(myTable);
 }
 
-function guardarInformacionBikes(){
+function guardarInformacionFarm() {
     let var3 = {
-        name:$("#Bname").val(),
-        brand:$("#Bbrand").val(),
-        year:$("#Byear").val(),
-        description:$("#Bdescription").val(),
-        };
-      
-        $.ajax({
-        type:'POST',
+        name: $("#Bname").val(),
+        address: $("#Baddress").val(),
+        extension: $("#Bextension").val(),
+        description: $("#Bdescription").val(),
+    };
+
+    $.ajax({
+        type: 'POST',
         contentType: "application/json; charset=utf-8",
         dataType: 'JSON',
         data: JSON.stringify(var3),
-        
-        url:"http://129.151.120.79:8080/api/Bike/save",
-       
-        
-        success:function(response) {
-                console.log(response);
+
+        url: "http://localhost:8080/api/Farm/save",
+
+
+        success: function (response) {
+            console.log(response);
             console.log("Se guardo correctamente");
             alert("Se guardo correctamente");
             window.location.reload()
-    
+
         },
-        
-        error: function(jqXHR, textStatus, errorThrown) {
-              window.location.reload()
+
+        error: function (jqXHR, textStatus, errorThrown) {
+            window.location.reload()
             alert("No se guardo correctamente");
-    
-    
+
+
         }
-        });
+    });
 
 }
 //////////////////////Clientes//////////////////////////////////
-function traerInformacionClientes(){
+function traerInformacionClientes() {
     $.ajax({
-        url:"http://129.151.120.79:8080/api/Client/all",
-        type:"GET",
-        datatype:"JSON",
-        success:function(respuesta){
+        url: "http://localhost:8080/api/Client/all",
+        type: "GET",
+        datatype: "JSON",
+        success: function (respuesta) {
             console.log(respuesta);
             pintarRespuestaClientes(respuesta);
         }
     });
 }
 
-function pintarRespuestaClientes(respuesta){
+function pintarRespuestaClientes(respuesta) {
 
-    let myTable="<table>";
-    for(i=0;i<respuesta.length;i++){
-        myTable+="<tr>";
-        myTable+="<td>"+respuesta[i].email+"</td>";
-        myTable+="<td>"+respuesta[i].password+"</td>";
-        myTable+="<td>"+respuesta[i].name+"</td>";
-        myTable+="<td>"+respuesta[i].age+"</td>";
-        myTable+="</tr>";
+    let myTable = "<table>";
+    for (i = 0; i < respuesta.length; i++) {
+        myTable += "<tr>";
+        myTable += "<td>" + respuesta[i].email + "</td>";
+        myTable += "<td>" + respuesta[i].password + "</td>";
+        myTable += "<td>" + respuesta[i].name + "</td>";
+        myTable += "<td>" + respuesta[i].age + "</td>";
+        myTable += "</tr>";
     }
-    myTable+="</table>";
+    myTable += "</table>";
     $("#resultado3").html(myTable);
 }
 
-function guardarInformacionClientes(){
+function guardarInformacionClientes() {
     let var4 = {
-        email:$("#CLemail").val(),
-        password:$("#CLpassword").val(),
-        name:$("#CLname").val(),
-        age:$("#CLage").val(),
-        };
-      
-        $.ajax({
-        type:'POST',
+        email: $("#CLemail").val(),
+        password: $("#CLpassword").val(),
+        name: $("#CLname").val(),
+        age: $("#CLage").val(),
+    };
+
+    $.ajax({
+        type: 'POST',
         contentType: "application/json; charset=utf-8",
         dataType: 'JSON',
         data: JSON.stringify(var4),
-        
-        url:"http://129.151.120.79:8080/api/Client/save",
-       
-        
-        success:function(response) {
-                console.log(response);
+
+        url: "http://localhost:8080/api/Client/save",
+
+
+        success: function (response) {
+            console.log(response);
             console.log("Se guardo correctamente");
             alert("Se guardo correctamente");
             window.location.reload()
-    
+
         },
-        
-        error: function(jqXHR, textStatus, errorThrown) {
-              window.location.reload()
+
+        error: function (jqXHR, textStatus, errorThrown) {
+            window.location.reload()
             alert("No se guardo correctamente");
-    
-    
+
+
         }
-        });
+    });
+
+}
+
+
+
+
+//////////////////////Mensaje//////////////////////////////////
+function traerInformacionMensaje() {
+    $.ajax({
+        url: "http://localhost:8080/api/Message/all",
+        type: "GET",
+        datatype: "JSON",
+        success: function (respuesta) {
+            console.log(respuesta);
+            pintarRespuestaMensaje(respuesta);
+        }
+    });
+}
+
+function pintarRespuestaMensaje(respuesta) {
+
+    let myTable = "<table>";
+    for (i = 0; i < respuesta.length; i++) {
+        myTable += "<tr>";
+        myTable += "<td>" + respuesta[i].messageText + "</td>";
+        myTable += "</tr>";
+    }
+    myTable += "</table>";
+    $("#resultado4").html(myTable);
+}
+
+function guardarInformacionMensaje() {
+    let var5 = {
+        messageText: $("#CLmessageText").val(),
+
+    };
+
+    $.ajax({
+        type: 'POST',
+        contentType: "application/json; charset=utf-8",
+        dataType: 'JSON',
+        data: JSON.stringify(var5),
+        url: "http://localhost:8080/api/Message/save",
+
+
+        success: function (response) {
+            console.log(response);
+            console.log("Se guardo correctamente");
+            alert("Se guardo correctamente");
+            window.location.reload()
+        },
+
+        error: function (jqXHR, textStatus, errorThrown) {
+            window.location.reload()
+            alert("No se guardo correctamente");
+        }
+       
+    });
+
+}
+
+
+  //////////////////////Reservacion//////////////////////////////////
+  function traerInformacionReservacion() {
+    $.ajax({
+        url: "http://localhost:8080/api/Reservation/all",
+        type: "GET",
+        datatype: "JSON",
+        success: function (respuesta) {
+            console.log(respuesta);
+            pintarRespuestaReservacion(respuesta);
+        }
+    });
+}
+
+function pintarRespuestaReservacion(respuesta) {
+
+    let myTable = "<table>";
+    for (i = 0; i < respuesta.length; i++) {
+        myTable += "<tr>";
+        myTable += "<td>" + respuesta[i].startDate + "</td>";
+        myTable += "<td>" + respuesta[i].devolutionDate + "</td>";
+        myTable += "<td>" + respuesta[i].status + "</td>";
+        myTable += "</tr>";
+    }
+    myTable += "</table>";
+    $("#resultado5").html(myTable);
+}
+
+function guardarInformacionReservacion() {
+    let var6 = {
+        startDate: $("#CLstartDate").val(),
+        devolutionDate: $("#CLdevolutionDate").val(),
+        status: $("#CLstatus").val()
+
+    };
+
+    $.ajax({
+        type: 'POST',
+        contentType: "application/json; charset=utf-8",
+        dataType: 'JSON',
+        data: JSON.stringify(var6),
+
+        url: "http://localhost:8080/api/Reservation/save",
+
+
+        success: function (response) {
+            console.log(response);
+            console.log("Se guardo correctamente");
+            alert("Se guardo correctamente");
+            window.location.reload()
+
+        },
+
+        error: function (jqXHR, textStatus, errorThrown) {
+            window.location.reload()
+            alert("No se guardo correctamente");
+        }
+    });
 
 }
